eadc8f477848e3ad510a1f810e8ca12a032e7c5a
HabibaMoulfi <kh_moulfi@esi.dz>
2024-05-14 14:03:42 +0100

    Fixing the get total duration script

--- scripts/get_total_duration_between_months.sql
+++ scripts/get_total_duration_between_months.sql
@@ -5,24 +5,19 @@ CREATE OR REPLACE FUNCTION get_total_duration_between_months (
 ) RETURN INTEGER AS
     total_duration NUMBER := 0;
 BEGIN
-    -- Initialize total_activities count to 0
-
-    -- Construct the start and end dates based on the provided month and year
-    DECLARE
-        start_date TIMESTAMP WITH TIME ZONE := TO_TIMESTAMP_TZ(TO_CHAR(p_year) || '-' || TO_CHAR(p_start_month, 'FM00') || '-01 00:00:00.000', 'YYYY-MM-DD HH24:MI:SS.FF3 TZR');
-        end_date TIMESTAMP WITH TIME ZONE := LAST_DAY(TO_TIMESTAMP_TZ(TO_CHAR(p_year) || '-' || TO_CHAR(p_end_month, 'FM00') || '-01 00:00:00.000', 'YYYY-MM-DD HH24:MI:SS.FF3 TZR'));
-    BEGIN
+    
         -- Count the number of activities between the constructed start and end dates
         SELECT SUM(MOVING_TIME) INTO total_duration
         FROM ACTIVITIES
         WHERE SPORT_TYPE = 'Run'
-            AND TRUNC(START_DATE_LOCAL, 'MI') BETWEEN TRUNC(start_date, 'MI') AND TRUNC(end_date, 'MI');
+            AND EXTRACT(YEAR FROM START_DATE_LOCAL) = p_year
+           AND EXTRACT(MONTH FROM START_DATE_LOCAL) BETWEEN p_start_month AND p_end_month;
 
         -- Convert duration from seconds to hours
     total_duration := total_duration / 3600; -- 3600 seconds in an hour
 
     RETURN total_duration;
-    END;
+
 END;
 
 
