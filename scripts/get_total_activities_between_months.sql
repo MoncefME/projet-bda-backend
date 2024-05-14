812d6ed4c8843461613c7ddd9d7702dd920d058d
HabibaMoulfi <kh_moulfi@esi.dz>
2024-05-14 14:01:23 +0100

    Fixing the get total activities script

--- scripts/get_total_activities_between_months.sql
+++ scripts/get_total_activities_between_months.sql
@@ -9,19 +9,17 @@ BEGIN
     total_activities := 0;
 
     -- Construct the start and end dates based on the provided month and year
-    DECLARE
-        start_date TIMESTAMP WITH TIME ZONE := TO_TIMESTAMP_TZ(TO_CHAR(p_year) || '-' || TO_CHAR(p_start_month, 'FM00') || '-01 00:00:00.000', 'YYYY-MM-DD HH24:MI:SS.FF3 TZR');
-        end_date TIMESTAMP WITH TIME ZONE := LAST_DAY(TO_TIMESTAMP_TZ(TO_CHAR(p_year) || '-' || TO_CHAR(p_end_month, 'FM00') || '-01 00:00:00.000', 'YYYY-MM-DD HH24:MI:SS.FF3 TZR'));
-    BEGIN
+    
         -- Count the number of activities between the constructed start and end dates
         SELECT COUNT(*)
         INTO total_activities
         FROM ACTIVITIES
         WHERE SPORT_TYPE = 'Run'
-            AND TRUNC(START_DATE_LOCAL, 'MI') BETWEEN TRUNC(start_date, 'MI') AND TRUNC(end_date, 'MI');
+            AND EXTRACT(YEAR FROM START_DATE_LOCAL) = p_year
+           AND EXTRACT(MONTH FROM START_DATE_LOCAL) BETWEEN p_start_month AND p_end_month;
 
         RETURN total_activities;
-    END;
+    
 END;
 
 SET SERVEROUTPUT ON;
@@ -29,7 +27,7 @@ DECLARE
     total_activities INTEGER;
 BEGIN
     -- Call the function with sample input values (start month, end month, year)
-    total_activities := get_total_activities_between_months(5, 5, 2023); -- May 2023 to May 2023
+    total_activities := get_total_activities_between_months(1, 12, 2022); -- May 2023 to May 2023
 
     -- Output the result
     DBMS_OUTPUT.PUT_LINE('Total activities between May 2023 and May 2023: ' || total_activities);
