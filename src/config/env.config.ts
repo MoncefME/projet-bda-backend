54120a912ebfa43e8ef7a79fde019a2b77b388b0
MoncefME <ka_moussaoui@esi.dz>
2024-05-15 22:22:34 +0100

    Remove commented out code in env.config.ts

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -3,12 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const ENV = {
-
-
-  // ORACLE_USER: process.env.ORACLE_USER,
-  // ORACLE_PASSWORD: process.env.ORACLE_PASSWORD,
-  // ORACLE_CONNECTION_STRING: process.env.ORACLE_CONNECTION_STRING,
-  // PORT: process.env.PORT,
   ORACLE_USER: process.env.ORACLE_USER,
   ORACLE_PASSWORD: process.env.ORACLE_PASSWORD,
   ORACLE_CONNECTION_STRING: process.env.ORACLE_CONNECTION_STRING,
