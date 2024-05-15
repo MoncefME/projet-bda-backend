c1e758979757cd2dc27b10cd6506978e71786b55
HabibaMoulfi <kh_moulfi@esi.dz>
2024-05-15 18:59:05 +0100

    Adding the get speed vs distance functionality

--- src/routes/activity.route.ts
+++ src/routes/activity.route.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from "express";
 import { getActivityById, getAllActivities } from "../handlers/activity";
+import { getSpeedVsDistance } from "../handlers/activity/get-speed-vs-distance";
 
 const activityRouter = Router();
 
@@ -28,5 +29,34 @@ activityRouter.get("/:id", async (req: Request, res: Response) => {
     res.status(500).json({ message: "Internal server error" });
   }
 });
+
+activityRouter.get("/speed-vs-distance/:activity_id", async (req: Request, res: Response) => {
+  try {
+      const { activity_id } = req.params;
+      const {startMonth, endMonth, year } = req.query;
+
+    if (!startMonth || !endMonth || !year) {
+      return res.status(400).json({ error: "startMonth, endMonth, and year are required" });
+    }
+
+      // Call the backend function to get speed vs. distance data
+      const speedVsDistance = await getSpeedVsDistance(
+        Number(activity_id),
+        parseInt(startMonth as string),
+        parseInt(endMonth as string),
+        parseInt(year as string)
+      );
+
+      if (speedVsDistance !== null) {
+          return res.status(200).json(speedVsDistance);
+      } else {
+          return res.status(500).json({ error: "Failed to fetch speed vs. distance data" });
+      }
+  } catch (error) {
+      console.error("Error:", error);
+      return res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 
 export default activityRouter;
