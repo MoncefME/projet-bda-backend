0ff9674e75432dfef3cc411c96bcb53bc3837a6b
MoncefME <ka_moussaoui@esi.dz>
2024-05-18 14:57:21 +0100

    updated import for activities

--- src/handlers/activity/index.ts
+++ src/handlers/activity/index.ts
@@ -1,4 +1,6 @@
 import { getAllActivities } from "./get-all-activities";
 import { getActivityById } from "./get-by-id";
+import { getSpeedVsDistance } from "./get-speed-vs-distance";
+import { createNewRun } from "./create-new-run";
 
-export { getActivityById, getAllActivities };
+export { getActivityById, getAllActivities, getSpeedVsDistance, createNewRun };
